316631d7102ea98d67971307a9e802bc4c07c9c9
Edd <edd_kleszcz@hotmail.co.uk>
2022-01-12 15:00:05 +0000

    Adding configuration

---
+++ sfg-di/src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -0,0 +1,20 @@
+package guru.springframework.sfgdi.controllers;
+
+import com.springframework.pets.PetService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PetController {
+
+    private final PetService petService;
+
+    @Autowired
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    public String whichPetIsTheBest(){
+        return petService.getPetType();
+    }
+}
